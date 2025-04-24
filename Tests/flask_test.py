d3a24f4c5176d677c2d213fbf20619d56e6debf3
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-04-23 22:52:38 -0500

    Update flask_test.py

--- Tests/flask_test.py
+++ Tests/flask_test.py
@@ -1,8 +1,7 @@
-import sys
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+import sys
+import unittest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import *
 import unittest
 
