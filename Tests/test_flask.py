e31d98b8c40955c475f34c3e162bda79bef5514b
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-04-25 16:41:28 -0500

    Update test_flask.py

--- Tests/test_flask.py
+++ Tests/test_flask.py
@@ -1,8 +1,9 @@
 ''' Test the Flask app routes and error handlers.'''
 import unittest
-from app import *
+from app import app, get_data
 
 class TestHomePage(unittest.TestCase):
+    ''' Testing the homepage'''
     def test_route(self):
         ''' Test the homepage route '''
         self.app = app.test_client()
@@ -14,6 +15,7 @@ class TestHomePage(unittest.TestCase):
         self.assertIn(homepage_text_bytes, response.data)
 
 class TestGetRandom(unittest.TestCase):
+    ''' Testing the get random capability'''
     def test_route(self):
         ''' Test the get_random route '''
         self.app = app.test_client()
@@ -23,6 +25,7 @@ class TestGetRandom(unittest.TestCase):
         self.assertEqual(random, response.data)
 
 class TestRoute404(unittest.TestCase):
+    ''' Testing the 404 error handler'''
     def test_route_404(self):
         ''' Test the 404 error handler '''
         self.app = app.test_client()
@@ -30,6 +33,7 @@ class TestRoute404(unittest.TestCase):
         self.assertEqual(b'Sorry, wrong format. Type /number after the current URL.', response.data)
 
 class TestRoute500(unittest.TestCase):
+    ''' Testing the 500 error handler'''
     def test_route_500(self):
         ''' Test the 500 error handler '''
         self.app = app.test_client()
@@ -38,4 +42,3 @@ class TestRoute500(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
