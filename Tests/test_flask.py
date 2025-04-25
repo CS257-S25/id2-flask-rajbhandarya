d15f4c35a5f93fb8c3efa413b33a3f2f587ea1b4
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-04-25 16:43:46 -0500

    Update test_flask.py

--- Tests/test_flask.py
+++ Tests/test_flask.py
@@ -4,9 +4,11 @@ from app import app, get_data
 
 class TestHomePage(unittest.TestCase):
     ''' Testing the homepage'''
+    def setUp(self):
+        """Set up before each test."""
+        self.app = app.test_client()
     def test_route(self):
         ''' Test the homepage route '''
-        self.app = app.test_client()
         response = self.app.get('/', follow_redirects=True)
         homepage_text_str = ("Hello, this is the homepage. To find a random recipe, "
                               "go type /number after the current URL where number "
@@ -16,9 +18,11 @@ class TestHomePage(unittest.TestCase):
 
 class TestGetRandom(unittest.TestCase):
     ''' Testing the get random capability'''
+    def setUp(self):
+        """Set up before each test."""
+        self.app = app.test_client()
     def test_route(self):
         ''' Test the get_random route '''
-        self.app = app.test_client()
         response = self.app.get('/1', follow_redirects=True)
         data = get_data()
         random = (' - '.join(map(str, data[9220]))).encode('utf-8')
@@ -26,17 +30,21 @@ class TestGetRandom(unittest.TestCase):
 
 class TestRoute404(unittest.TestCase):
     ''' Testing the 404 error handler'''
+    def setUp(self):
+        """Set up before each test."""
+        self.app = app.test_client()
     def test_route_404(self):
         ''' Test the 404 error handler '''
-        self.app = app.test_client()
         response = self.app.get('/0/1/2', follow_redirects=True)
         self.assertEqual(b'Sorry, wrong format. Type /number after the current URL.', response.data)
 
 class TestRoute500(unittest.TestCase):
     ''' Testing the 500 error handler'''
+    def setUp(self):
+        """Set up before each test."""
+        self.app = app.test_client()
     def test_route_500(self):
         ''' Test the 500 error handler '''
-        self.app = app.test_client()
         response = self.app.get('/a', follow_redirects=True)
         self.assertEqual(b'Eek, a bug!', response.data)
 
