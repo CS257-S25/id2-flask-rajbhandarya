e14a8de31c56efec44a244c034963c2d826da2ba
Anika Rajbhandary <126826820+rajbhandarya@users.noreply.github.com>
2025-04-25 16:51:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''Replace me with your flask app'''
+''' This is the main file of the Flask app.'''
 from flask import Flask
 from ProductionCode.data import get_data
 from ProductionCode.random_recipe import get_random_recipes
@@ -10,12 +10,18 @@ def homepage():
     return "Hello, this is the homepage. To find a random recipe, "+ \
         "go type /number after the current URL where number " + \
               "is the amount of random recipes you want."
+
 @app.route('/<number>', strict_slashes=False)
 def get_random(number):
     ''' Returns a random recipe from the list of recipes. '''
     data = get_data()
-    random = get_random_recipes(data, int(number))
-    recipes_as_strings = [' - '.join(map(str, recipe)) for recipe in random]
+    recipes = get_random_recipes(data, int(number))
+    random = parse_random(recipes)
+    return random
+
+def parse_random(recipes):
+    ''' Parses the random recipes into a string. '''
+    recipes_as_strings = [' - '.join(map(str, recipe)) for recipe in recipes]
     return "<br>----------------------------------<br>".join(recipes_as_strings)
 
 @app.errorhandler(404)
